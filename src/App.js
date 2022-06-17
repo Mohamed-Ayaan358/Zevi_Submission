6e5160af602364a16abc1d616f55efe8c5f06e0c
Mohamed Ayaan <ayaan35813@gmail.com>
2022-06-17 18:17:58 +0530

    Finished i suppose

--- src/App.js
+++ src/App.js
@@ -213,25 +213,3 @@ function App() {
 export default App;
 
 
-{/* 
-
-// <article className="card" key={item.upc}>
-//   <div className="card-image">
-//     <img src={item.image} alt={item.image} />
-//   </div>
-//   <div className="card-content">
-//     <h2 className="card-name">{item.name}</h2>
-//     <ol className="card-list">
-//       <li>
-//         population:{" "}
-//         <span>{item.population}</span>
-//       </li>
-//       <li>
-//         Region: <span>{item.region}</span>
-//       </li>
-//       <li>
-//         Capital: <span>{item.capital}</span>
-//       </li>
-//     </ol>
-//   </div>
-// </article> */}
